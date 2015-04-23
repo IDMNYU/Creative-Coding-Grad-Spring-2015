33c494ad7fb02010dbb2381f77f02b6b2f986f13
indefinit <kevin.siwoff@gmail.com>
2015-04-23 19:17:22 -0400

    adds week 12 code

--- week_11/nytimes_api_example/sketch.js
+++ week_11/nytimes_api_example/sketch.js
@@ -3,24 +3,46 @@ var currentLineNum = 0;
 var currentLine;
 var lifetime = 255;
 
+var docs;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
+
+/*
+http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?
+callback=svc_search_v2_articlesearch
+&q=jamaica
+&api-key=7384cc04f466965ccf07bc8dc93afb22%3A18%3A66115623
+
+ */
+
+
 		// replace this api key with your own from developer.nytimes.com
 	var apiKey = '7384cc04f466965ccf07bc8dc93afb22%3A18%3A66115623';
+	var query = 'Apple';
 
 	//load json from the NYTimes search api.  
 	// Notice how we are adding a function callback after the request has completed.
 	// try replacing function(data){} with a call to existing function in your code
-	loadJSON('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Obama&api-key='+ apiKey, function(data){
+	loadJSON('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&api-key='+ apiKey, function(data){
 		//since our data is a javascript object, we can use dot notation on it
 		//to drill deeper into the data structure
-		var docs = data.response.docs;
+		docs = data.response.docs;
 		for(var article in docs){
-			//push the lead paragraph string into our leadLines array
-			leadLines.push(docs[article].lead_paragraph);
+			
+			//if the lead paragraph exists
+			//
+			if(docs[article].lead_paragraph){
+				//push the lead paragraph string into our leadLines array
+				leadLines.push(docs[article].lead_paragraph);
+			}	
+
 		}
+
+		//same as console.log(leadLines);
 		println(leadLines);
+		
 		currentLine = leadLines[currentLineNum];
 
 	});
@@ -29,6 +51,7 @@ function setup() {
 
 function draw() {
 	background(255);
+
 	if (currentLine){
 		if ( lifetime <=0 ){
 			currentLineNum++; //increment to next line
@@ -37,6 +60,29 @@ function draw() {
 			currentLine = leadLines[currentLineNum];
 			lifetime = 255;
 		}
+		var dateArray = split(docs[currentLineNum].pub_date, "-");
+		
+		var month = dateArray[1];
+		switch(month){
+			case '01':
+				month = "Jan";
+				break;
+			case '04':
+				month = "Apr";
+				break;
+		}
+
+		//the day looks like:
+		//09T07:57:47Z
+		var day = dateArray[2].substring(0,2);
+
+		var newDate =  month + " " +  day + ", " + dateArray[0];
+		
+		fill(0);
+		textSize(18);
+		text(newDate, 25, 25);
+		// println(docs[currentLineNum].pub_date);
+
 		textAlign(CENTER);
 		textSize(18);
 		fill(255-lifetime);
@@ -47,6 +93,7 @@ function draw() {
 		
 		text(currentLine, width / 2, mappedTextPosY , 500, 500);
 		
+
 		lifetime--;
 	}
 }
---
+++ week_11/p5js_translation/sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+
+  createCanvas(500,500);
+}
+
+function draw(){
+  arc(width /2, height / 2, 50,50, radians(180), TWO_PI);
+  ellipse(width /2 , height / 2, 50, 50                          );
+}
